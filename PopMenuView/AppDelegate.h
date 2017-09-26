886f269029293cafe4e4bb84a69c2f4eb4b8d244
AlexCorleone <1034131730@qq.com>
2017-09-26 23:25:45 +0800

    Initial Commit

---
+++ PopMenuView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopMenuView
+//
+//  Created by AlexCorleone on 2017/9/26.
+//  Copyright © 2017年 AlexCorleone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopMenuView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PopMenuView
+//
+//  Created by AlexCorleone on 2017/9/26.
+//  Copyright © 2017年 AlexCorleone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
